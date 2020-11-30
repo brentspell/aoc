8f8199c200278f3bbec71189d837a8abface29b1
Brent M. Spell <bspell1@gmail.com>
2020-11-30 17:05:00 -0500

    initial revision

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::env;
+
+const SOLUTIONS: &[&dyn Fn()] = &[];
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        2 => dispatch(args[1].parse().unwrap()),
+        _ => (1..=SOLUTIONS.len()).map(dispatch).collect(),
+    }
+}
+
+fn dispatch(day: usize) {
+    println!("day {}", day);
+    match SOLUTIONS.get(day - 1) {
+        Some(f) => f(),
+        None => println!("not implemented yet"),
+    }
+    println!()
+}
