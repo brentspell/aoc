35e2ce1785aaab19a4c9e52494117693aa98dc5e
Brent M. Spell <bspell1@gmail.com>
2023-11-30 06:19:53 -0500

    2023 project setup

---
+++ 2023/src/main.rs
@@ -0,0 +1,24 @@
+use std::env;
+
+//mod day01;
+
+const SOLUTIONS: &[&dyn Fn()] = &[
+    //&day01::solve,
+];
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        2 => dispatch(args[1].parse().unwrap()),
+        _ => (1..=SOLUTIONS.len()).map(dispatch).collect(),
+    }
+}
+
+fn dispatch(day: usize) {
+    println!("day {}", day);
+    match SOLUTIONS.get(day - 1) {
+        Some(f) => f(),
+        None => println!("not implemented yet"),
+    }
+    println!()
+}
--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 This repo contains my solutions to the [Advent of Code](https://adventofcode.com/events) challenges, in reverse completion order.
 
-* [2021](https://adventofcode.com/2022): Rust
+* [2023](https://adventofcode.com/2023): Rust
+* [2022](https://adventofcode.com/2022): Rust
 * [2021](https://adventofcode.com/2021): Python/Jupyter
 * [2017](https://adventofcode.com/2017): Python (with tests!)
 * [2016](https://adventofcode.com/2016): TypeScript
